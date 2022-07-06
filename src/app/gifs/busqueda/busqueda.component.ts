19ffbf3ce06b71ccfadb296bbd0c1315d315649d
samuel-pnj <samuelperezgamez@outlook.com>
2022-07-06 12:15:15 +0200

    Cuarto commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,24 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs-service.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: []
+})
+export class BusquedaComponent {
+
+@ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+constructor( private gifsService: GifsService){}
+
+buscar(){
+ 
+ const valor = this.txtBuscar.nativeElement.value;
+ 
+  this.gifsService.buscarGifs( valor );
+
+ this.txtBuscar.nativeElement.value= '';  
+}
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent
+  ], exports: [
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
